f5be4ecce2022587dda1d7c8d16d04012fb649a8
el-sebas-galeano <jsebastian-galeano@javeriana.edu.co>
2024-10-12 03:19:59 -0500

    :sparkles: feat: Creación de la Aplicación SpringBoot.

---
+++ ms-conexiones-externas/src/main/java/com/rancho_smart/ms_conexiones_externas/MsConexionesExternasApplication.java
@@ -0,0 +1,13 @@
+package com.rancho_smart.ms_conexiones_externas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsConexionesExternasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsConexionesExternasApplication.class, args);
+	}
+
+}
---
+++ ms-conexiones-externas/src/test/java/com/rancho_smart/ms_conexiones_externas/MsConexionesExternasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rancho_smart.ms_conexiones_externas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsConexionesExternasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
