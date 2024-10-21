6c0829e2a0416bd56f7628c0d8303747475351bb
el-sebas-galeano <jsebastian-galeano@javeriana.edu.co>
2024-10-21 03:27:32 -0500

    :wrench: chore: Configuración con Docker y Docker Compose

--- src/main/java/com/rancho_smart/ms_conexiones_externas/MsConexionesExternasApplication.java
+++ src/main/java/com/rancho_smart/ms_conexiones_externas/MsConexionesExternasApplication.java
@@ -2,8 +2,10 @@ package com.rancho_smart.ms_conexiones_externas;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class MsConexionesExternasApplication {
 
 	public static void main(String[] args) {
